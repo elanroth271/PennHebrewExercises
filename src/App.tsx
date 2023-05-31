6a9e28027ba6f0b5df744b2fafa6957bb2a7cbf5
Lior Ben-David <liorbd@Outlook.com>
2023-05-30 22:33:26 -0400

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+type queryResult = {
+  _type: string,
+  name: string,
+  course: any,
+  _id: string
+}
+
+
+type quizRef = {
+  name: string,
+  _id: string
+}
+
+
 function App() {
+  
+  const [courses, setCourses] = useState<Record<string, quizRef[]>>({})
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${process.env.REACT_APP_SANITY_COURSES_URL}`);
+        if (!response.ok) {
+          throw new Error("HTTP error " + response.status);
+        }
+        const data = await response.json();
+        let tempCourses: Record<string, quizRef[]> = {}
+        data.result.forEach((res: queryResult) => {
+          if(res._type === "course") {
+            tempCourses[res.name] = []
+          } else{
+            tempCourses[res.course.name].push({name: res.name, _id: res._id})
+          }
+        })
+        setCourses(tempCourses)
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {Object.entries(courses).map(([key, value]) => {
+        return <div>
+          <h2>{key}</h2>
+          {value.map((quiz) => {
+            return <div>{quiz.name}</div>
+          })}
+        </div>
+      })}
     </div>
   );
 }
